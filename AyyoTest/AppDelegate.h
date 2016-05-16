53d793ada3deb72a8820b01003768d68bdb41740
Semyon Belokovsky <igrampe@gmail.com>
2016-05-16 21:51:55 +0500

    Initial Commit

---
+++ AyyoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AyyoTest
+//
+//  Created by Semyon Belokovsky on 16/05/16.
+//  Copyright © 2016 igrampe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AyyoTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AyyoTest
+//
+//  Created by Semyon Belokovsky on 16/05/16.
+//  Copyright © 2016 igrampe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
