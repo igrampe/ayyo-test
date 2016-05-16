a094a0e1924b0f9f4a3cab9754d666587b1fcad9
Semyon Belokovsky <igrampe@gmail.com>
2016-05-16 22:57:25 +0500

    APL

---
+++ AyyoTest/Resources/ColorSchemeHeader.h
@@ -0,0 +1,17 @@
+//
+//  ColorSchemeHeader.h
+//  SkyEngTest
+//
+//  Created by Semyon Belokovsky on 18/04/16.
+//  Copyright © 2016 igrampe. All rights reserved.
+//
+
+#ifndef ColorSchemeHeader_h
+#define ColorSchemeHeader_h
+
+#define SSC(x, y) static NSString *const x = y;
+
+SSC(Color_White, @"White")
+SSC(Color_Black, @"Black")
+
+#endif /* ColorSchemeHeader_h */
